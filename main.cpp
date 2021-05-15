97e1d91f06121b44408da20daf58db86cb6d220c
Kestoc <kevin512ocampo@gmail.com>
2021-05-15 15:25:25 -0500

    Archivos base e implementaciones

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include "priority.h"
+using namespace std;
+
+int main(){
+    /*Process proc[] = {{1, 10, 2}, {2, 5, 0}, {3, 8, 1}};
+    int n = sizeof proc / sizeof proc[0];
+    priorityScheduling(proc, n);*/
+    return 0;
+}
